fa770e1d0d057c82b6cac79e6a769dcba8e2e0fa
lifeoflikhon <mehedi.hasansjs@gmail.com>
2022-02-28 22:43:23 +0600

    get all users

--- CloudCustomers.API/Program.cs
+++ CloudCustomers.API/Program.cs
@@ -1,5 +1,8 @@
+using CloudCustomers.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+ConfigureServices(builder.Services);
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -23,3 +26,9 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddTransient<IUsersService, UsersService>();
+}
